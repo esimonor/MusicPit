9a46492f93eb18f9ba2b046d5c6c473374aae89b
esimonor <esimonor18dw@ikzubirimanteo.com>
2020-12-07 13:26:22 +0100

    Fixed the modals and added some JQuery animations

---
+++ public/JS/fadeScript.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("#about").click(function(){
+      $(".content").fadeOut("slow");
+      $(".about").delay(666).fadeIn("fast");
+    });
+    $("#home").click(function(){
+        $(".about").fadeOut("slow");
+        $(".content").delay(666).fadeIn("fast");
+      });
+  });
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,6 +13,11 @@
     <!-- Fonts -->
     <link href='https://fonts.googleapis.com/css?family=Cambay' rel='stylesheet'>
     <title>FINAL PROYECT - LANDING PAGE [Eneko]</title>
+
+	<!-- Scripts -->
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="JS/fadeScript.js"></script>
   </head>
   <body>
   	<!-- navbar -->
@@ -26,13 +31,13 @@
 	  <div class="collapse navbar-collapse" id="navbarNavDropdown">
 	    <ul class="navbar-nav">
 	      <li class="nav-item active">
-	        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+	        <a class="nav-link" id="home" href="#">Home <span class="sr-only">(current)</span></a>
 	      </li>
 	      <li class="nav-item">
-	        <a class="nav-link" href="#about">About MusicPit</a>
+	        <a class="nav-link" id="about" href="#about">About MusicPit</a>
 	      </li>
 	      <li class="nav-item">
-	        <a class="nav-link" href="#">Contact</a>
+	        <a class="nav-link" href="#" data-toggle="modal" data-target="#LoginModal">Login</a>
 	      </li>
 	      <li class="nav-item dropdown">
 	        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -58,9 +63,15 @@
 		<p>The ultimate social media to find other musicians and jam together</p>
 		<p><b>Start jamming with musicians near you TODAY!</b></p>
 		<button type="button" class="btn btn-dark btn-lg CallToAction" data-toggle="modal" data-target="#exampleModal">Create Account</button>
-		<p><a data-toggle="modal" data-target="#exampleModal">Already have an account? Click here to log in</a></p>
+		<p><a data-toggle="modal" data-target="#LoginModal">Already have an account? Click here to log in</a></p>
 	</div>
-	<!-- MODAL -->
+	<!-- hidden overlay text -->
+	<div class="about">
+		<h1>About</h1>
+		<h3>MusicPit is a social media for musicians!</h3>
+		<p>Whether you are a professional musician or just like to jam with some people this is the page for you, with people for all around the globe you can find anyone anywhere to form a band, or just play some tunes for fun!</p>
+	</div>
+	<!-- CREATE ACCOUNT MODAL -->
 	<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
 	  <div class="modal-dialog" role="document">
 	    <div class="modal-content">
@@ -97,13 +108,41 @@
 	    </div>
 	  </div>
 	</div>
-	<!-- END MODAL -->
-	<!-- <div id="about">
-		<h3>MusicPit is a social media for musicians!</h3>
-		<p>Whether you are a professional musician or just like to jam with some people this is the page for you, with people for all around the globe you can find anyone anywhere to form a band, or just play some tunes for fun!</p>
-	</div> -->
+	<!-- END CREATE ACCOUNT MODAL -->
+
+	<!-- Login MODAL -->
+	<div class="modal fade" id="LoginModal" tabindex="-1" role="dialog" aria-labelledby="LoginModal" aria-hidden="true">
+	  <div class="modal-dialog" role="document">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <h5 class="modal-title" id="exampleModalLabel">Login</h5>
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	          <span aria-hidden="true">&times;</span>
+	        </button>
+	      </div>
+	      <div class="modal-body">
+	        <form>
+	          <div class="form-group">
+	            <label for="recipient-name" class="col-form-label">Name:</label>
+	            <input type="text" class="form-control">
+	          </div>
+	          <div class="form-group">
+	            <label for="message-text" class="col-form-label">Password:</label>
+	            <input type="password" class="form-control">
+	          </div>
+	        </form>
+	      </div>
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+	        <button type="button" class="btn btn-primary">Create account</button>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+	<!-- END Login MODAL -->
+
     <!-- jQuery, Popper.js, and Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"></script>
     
