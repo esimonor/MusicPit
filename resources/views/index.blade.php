83b17b5129962286fa0256cb6bf0d347724d740c
esimonor <esimonor18dw@ikzubirimanteo.com>
2020-12-09 11:39:23 +0100

    Fixed navbar bug that would not show 'home' tab

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -31,7 +31,7 @@
 	  <div class="collapse navbar-collapse" id="navbarNavDropdown">
 	    <ul class="navbar-nav">
 	      <li class="nav-item active">
-	        <a class="nav-link" class="home">Home <span class="sr-only">(current)</span></a>
+	        <a class="nav-link home">Home <span class="sr-only">(current)</span></a>
 	      </li>
 	      <li class="nav-item">
 	        <a class="nav-link" id="about">About</a>
