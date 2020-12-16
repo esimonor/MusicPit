9d08fe398c279f4fccb20cbde0f7cb1b6d58f744
esimonor <esimonor18dw@ikzubirimanteo.com>
2020-12-16 17:19:44 +0100

    Added localization path

---
+++ public/js/validate.js
@@ -0,0 +1,28 @@
+window.onload = start;
+
+function start(){
+    var allForms = document.getElementsByTagName("form");
+    for (let i = 0; i < allForms.length; i++) {
+        if(i == 1){
+            allForms[i].addEventListener('keyup', validate);
+        }
+    }
+}
+function validate(){
+    let username = document.getElementById("InputUsername").value;
+    let email = document.getElementById("InputEmail").value;
+    let password = document.getElementById("InputPassword1").value;
+    let confirm = document.getElementById("InputPassword2").value;
+    let pattern = /.*[@].*[.].*/;
+    var boton = document.getElementById("AccountButton");
+    
+    if(username == "" || email == "" || password == "" || confirm == "" ){
+        boton.disabled = true;
+    }else{
+        if(password == confirm && password.length >= 8 && pattern.test(email) == true){
+            boton.disabled = false;
+        } else{
+            boton.disabled = true;
+        }
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('welcome');
+
+Route::get('/{lang}', function ($lang) {
+    App::setlocale($lang);
+    return view('welcome');
+})->name('welcome');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
