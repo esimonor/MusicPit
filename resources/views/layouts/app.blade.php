2bb4382ab4038afe46c6b97e5380147cb1a09063
esimonor <esimonor18dw@ikzubirimanteo.com>
2021-01-13 13:17:31 +0100

    After verifying email it will redirect the user to the profile view and not the default jetstream view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,37 +7,196 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
+       <!-- Fonts -->
+       <link href="{{ URL::asset('https://fonts.googleapis.com/css?family=Muli:300,400,700,900') }}" rel="stylesheet">
+        <link rel="stylesheet" href="{{ URL::asset('/fonts/icomoon/style.css') }}">
+        <link rel="stylesheet" href="{{ URL::asset('/fonts/flaticon/font/flaticon.css') }}">
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-
-        @livewireStyles
+        <link rel="stylesheet" href="{{ URL::asset('/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ URL::asset('/css/jquery-ui.css') }}">
+        <link rel="stylesheet" href="{{ URL::asset('/css/style.css') }}">
+        <link rel="stylesheet" href="{{ URL::asset('/css/botones.css') }}">
+        <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.5.0/css/flag-icon.min.css"/> -->
+        <link rel="stylesheet" href="{{ URL::asset('/css/profile.css') }}">
 
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+    <body id="background-image" class="font-sans antialiased">
+         <!-- NAVBAR -->
+         <nav class="navbar navbar-expand-lg navbar-dark bg-dark m-2 p-2">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-            @livewire('navigation-dropdown')
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div class="site-logo w-20">
+                        <a>
+                            <img src="{{URL::asset('images/logo-transparent-invcolors.png')}}" width="100" height="60" class="d-inline-block align-top" alt="MusicPit">
+                        </a>
+                    </div>
+                <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="{{ route('welcome') }}">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Search Users</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Filter by:
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Instrument</a>
+                        <a class="dropdown-item" href="#">Music genre</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Localization</a>
+                    </div>
+                </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+                <form style="margin-left:30%" method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <a style="color:white;float:right;" href="http://127.0.0.1:8000/logout" onclick="event.preventDefault(); this.closest('form').submit();">Logout</a>
+                </form>
+            </div>
+        </nav>
+        <!-- END NAVBAR -->
+        <!--Profile body-->
+        <div class="container">
+            <div class="row gutters">
+                <div class="col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <div class="account-settings">
+                                <div class="user-profile">
+                                    <div class="user-avatar">
+                                        <img src="{{Auth::user()->profile_photo_path}}" alt="{{Auth::user()->name}}">
+                                    </div>
 
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
+                                    <h5 class="user-name">{{Auth::user()->name}}</h5>
+                                    <h6 class="user-email">{{Auth::user()->email}}</h6>
+
+                                </div>
+                                <div class="about">
+                                    <h5 class="mb-2 text-danger">About</h5>
+                                    <p>User description (gear, what kind of band, years of experience, etc)</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </header>
+                <div class="col-xl-9 col-lg-9 col-md-12 col-sm-12 col-12">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <div class="row gutters">
+                                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                    <h6 class="mb-3 text-danger">Details</h6>
+                                </div>
+                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                                    <div class="form-group">
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+                                        <label for="fullName">Name</label>
+                                        <h4 id="fullName">{{Auth::user()->name}}</h4>
+
+                                    </div>
+                                </div>
+                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                                    <div class="form-group">
+                                        <label for="eMail">Email</label>
+                                        <h4 id="eMail">{{Auth::user()->email}}</h4>
+                                    </div>
+                                </div>
+                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                                    <div class="form-group">
+                                        <label for="phone">Instrument</label>
+                                        <h4 id="instrument">{{Auth::user()->instrument}}</h4>
+                                    </div>
+                                </div>
+                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                                    <div class="form-group">
+                                        <label for="website">Music</label>
+                                        <h4 id="music">{{Auth::user()->music_genre}}</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row gutters">
+                                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                    <h6 class="mb-3 text-danger">Media</h6>
+                                </div>
+                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                                    <div class="form-group">
+                                        <label for="Street">Video</label>
+                                        <h4 id="video">No video yet</h4>
+                                    </div>
+                                </div>
+                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                                    <div class="form-group">
+                                        <label for="ciTy">Audio</label>
+                                        <h4 id="audio">No audio yet</h4>
+                                    </div>
+                                </div>
+                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                                    <div class="form-group">
+                                        <label for="sTate">Localization</label>
+                                        <h4 id="localization">No localization set</h4>
+                                    </div>
+                                </div>
+                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                                    <div class="form-group">
+                                        <label for="zIp">On a band?</label>
+                                        <h4 id="band">No</h4>
+                                    </div>
+                                </div>
+
+                                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                    <h6 class="mb-3 text-danger">Update or Delete your account</h6>
+                                </div>
+                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                                    <div class="form-group">
+                                    <button type="button" id="submit" name="submit" class="btn btn-success">Update</button>
+                                    </div>
+                                </div>
+
+                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                                    <div class="form-group">
+                                    <form action="{{ route('users.destroy', Auth::user()->id) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" id="submit" name="submit" class="btn btn-danger">Delete</button>
+                                    </form>
+
+                                    </div>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+    <!-- end profile body -->
 
-        @stack('modals')
-
+    <!-- Scripts -->
+    <script src="{{ URL::asset('/js/jquery-3.3.1.min.js') }}"></script>
+    <script src="{{ URL::asset('/js/jquery-migrate-3.0.1.min.js') }}"></script>
+    <script src="{{ URL::asset('/js/jquery-ui.js') }}"></script>
+    <script src="{{ URL::asset('/js/popper.min.js') }}"></script>
+    <script src="{{ URL::asset('/js/bootstrap.min.js') }}"></script>
+    <script src="{{ URL::asset('/js/owl.carousel.min.js') }}"></script>
+    <script src="{{ URL::asset('/js/jquery.stellar.min.js') }}"></script>
+    <script src="{{ URL::asset('/js/jquery.countdown.min.js') }}"></script>
+    <script src="{{ URL::asset('/js/bootstrap-datepicker.min.js') }}"></script>
+    <script src="{{ URL::asset('/js/jquery.easing.1.3.js') }}"></script>
+    <script src="{{ URL::asset('/js/aos.js') }}"></script>
+    <script src="{{ URL::asset('/js/jquery.fancybox.min.js') }}"></script>
+    <script src="{{ URL::asset('/js/jquery.sticky.js') }}"></script>
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+    <script src="{{ URL::asset('/js/main.js') }}"></script>
+    <script src="{{ URL::asset('/js/validate.js') }}"></script>
         @livewireScripts
     </body>
 </html>
